85dc171a96c9462d1e235e51ecced8573c2d8314
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-10 18:56:28 +0330

    install tailwindcss plugins

--- src/components/Contacts/AddContact.jsx
+++ src/components/Contacts/AddContact.jsx
@@ -18,30 +18,36 @@ const AddContact = () => {
 
   return (
     <div className="bg-white rounded-md shadow-lg px-6 py-2 w-1/3 text-gray-600    ">
-        <div  >
-            <div className="text-center">
-                <h1 className="text-2xl font-semibold py-4 mb-3">
-                    Add New Contact
-                </h1>
-            </div>
-            <div>
-                <form className="flex flex-col space-y-5">
-                    <lable>
-                        Fullname:
-                    </lable>
-                    <input type={'text'} placeholder='full name ' required />
-                    <lable>
-                        email:
-                    </lable>
-                    <input type={'email'} placeholder='email ' required />
-                    <lable>
-                        Phone:
-                    </lable>
-                    <input type={'tel'} placeholder='phone ' required />
-                    
-                </form>
-            </div>
+      <div>
+        <div className="text-center">
+          <h1 className="text-2xl font-semibold py-4 mb-3">Add New Contact</h1>
         </div>
+        <div>
+          <form className="flex flex-col ">
+            <lable>Fullname:</lable>
+            <input
+              type={"text"}
+              placeholder="full name "
+              required
+              className="rounded-lg border-gray-300 shadow-sm"
+            />
+            <lable>email:</lable>
+            <input
+              type={"email"}
+              placeholder="email "
+              required
+              className="rounded-lg border-gray-300 shadow-sm"
+            />
+            <lable>Phone:</lable>
+            <input
+              type={"tel"}
+              placeholder="phone "
+              required
+              className="rounded-lg border-gray-300 shadow-sm"
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
