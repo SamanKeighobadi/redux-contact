6eca2025f424741fb873063ddc58333b11f0c1d2
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-10 17:06:34 +0330

    init redux create store and import

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
