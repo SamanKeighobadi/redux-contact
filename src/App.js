f43578a774c56f2424cc924cbfb5dbf44b8e0a0d
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-10 17:48:54 +0330

    routing and create  home page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React from "react";
 import {ToastContainer} from 'react-toastify'
 // layouts
 import MainLayout from "./components/layout/MainLayout";
+// React Router DOM 
+import {Route,Routes} from 'react-router-dom'
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
       <MainLayout>
-        <div className="max-h-screen">
-          <h1 className="text-3xl text-blue-500">Hello </h1>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addContact" />
+          <Route path="/edit/:id" />
+        </Routes>
       </MainLayout>
       <ToastContainer />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {render} from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
 
 render(
-    <App />,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
